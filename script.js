f671bb28078b5196a6bdac060838199b449e3638
Alex <alex-admin@Ubuntu.myguest.virtualbox.org>
2023-05-01 20:04:28 -0400

    Added Ship class and instances + started game board function

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const colors = {
     '1' : 'red' , //player
     '-1' : 'blue' , //cpu
     '0' : 'grey' //blank 
-}
+};
 
 /*----- state variables -----*/
 let turn; // set turn
@@ -12,21 +12,39 @@ let cpuBoard; // set cpuboard
 let playerScore; // set playerScore
 let cpuScore; // set cpuScore
 let winner; //set winner 
-    // set ship class
-    //   extend ship class with three types and sizes of ships worth different points
+
     
 /*----- cached elements  -----*/
     // set playerScoreEl
-const playerScoreEl = document.querySelector('#playerScore')
+const playerScoreEl = document.querySelector('#playerScore');
     // set cpuScoreEl
-const cpuScoreEl = document.querySelector('#cpuScore')
+const cpuScoreEl = document.querySelector('#cpuScore');
     // set playAgainBtn
-const playAgainBtn = document.querySelector('button')
+const playAgainBtn = document.querySelector('#playAgain');
     // set gridButton * Each place on playerBoard needs to be clickable *
+const coordinate = 0;
+
+
+
+    // set ship class + extend ship class with three types and sizes of ships worth different points
+class Ship {
+    constructor(name, size, points){
+        this.name = name;
+        this.size = size;
+        this.points = points;
+    }
+}
+const boat = new Ship("Boat", 1, 1)
+const warship = new Ship ("Warship", 2, 2)
+const battleship = new Ship ("Battleship", 3, 3)
+
+console.log (warship)
+
 /*----- event listeners -----*/
     //   playAgainBtn event listener
+playAgainBtn.addEventListener('click', begin);
     // playerBoardBtn event listeners
-    
+
 /*----- functions -----*/
     // init {
     //   set turn to 1
@@ -37,7 +55,25 @@ const playAgainBtn = document.querySelector('button')
     //   player ship location modal
     //   render
     // }
-    
+fucntion createBoard () {
+    const sectionPlayerBoard = document.createElement("section")]
+    const sectionCpuBoard = document.createElement("section")
+    sectionPlayerBoard.setAttribute("id", "playerBoard")
+    sectionCpuBoard.setAttribute("id", "cpuBoard")
+    for (i=0; i < 11; i++) {
+    sectionCpuBoard.createElement(
+
+    )
+    sectionPlayerBoard.createElement(
+        
+    )
+    }
+
+}
+
+function begin () {
+    turn = "1";
+}
     // ship location modal {
     //   prompt player to choose ship locations
     //   push to game board
